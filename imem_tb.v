32e01d34169e0504fa14719f2723155596a9172b
Ethan Glover <glover.ethan@gmail.com>
2014-11-10 16:33:45 -0600

    IMEM Testbench (Jonathan)
    Uploaded from E-mail

---
+++ imem_tb.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+module imem_tb;
+
+reg [31:0] Address; 
+wire [31:0] Instruction;
+
+imem dut(.Address(Address), .Instruction(Instruction));
+
+initial begin
+	#10
+	Address = 0;
+	#10
+	Address = 4;
+	#10
+	Address = 8;
+	#10
+	Address = 12;
+	#10
+	Address = 16;
+	#10
+	Address = 20;
+end
+
+initial 
+	#70 $finish
+
+initial 
+	$monitor($time, "Address = %d", Address, "Instruction = %d", Instruction);
+
+endmodule
