ded7929141a9637059c2af8a94b01cd1e28e5dd1
Ethan Glover <glover.ethan@gmail.com>
2014-11-11 08:51:18 -0600

    IMEM tb Final

--- imem_tb.v
+++ imem_tb.v
@@ -1,30 +1,30 @@
 `timescale 1ns/1ps
 module imem_tb;
-
-reg [31:0] Address; 
-wire [31:0] Instruction;
-
-imem dut(.Address(Address), .Instruction(Instruction));
-
-initial begin
-	#10
-	Address = 0;
-	#10
-	Address = 4;
-	#10
-	Address = 8;
-	#10
-	Address = 12;
-	#10
-	Address = 16;
-	#10
-	Address = 20;
-end
-
-initial 
-	#70 $finish;
-
-initial 
-	$monitor($time, "Address = %d", Address, "Instruction = %d", Instruction);
-
+	// Inputs
+	reg clk;
+	reg [2:0] Address;
+	reg [3:0] index;
+	// Outputs
+	wire [31:0] Instruction;
+	
+	imem dut(.clk(clk),
+		.Address(Address),
+		.Instruction(Instruction)
+	);
+	
+	initial begin
+		clk = 0;
+		Address = 0;
+		
+		// Read and display all instructions currently in memory
+		for(index = 0; index < 8; index = index+1) begin
+			Address = index;
+			#10;
+			$display($time," Address = %d",Address," Instruction = %d",Instruction);
+		end
+	end
+	always#1 clk = ~clk; // clk is always current
+	
+	initial
+		#80 $finish;
 endmodule
